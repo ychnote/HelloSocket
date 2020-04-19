4cbbc406f8987a33d9a13397a380b44044f6b498
YCH-caihui <chyangbast@yeah.net>
2020-04-19 23:19:17 +0800

    服务端select模型

--- EsasyTcpSocket/server.cpp
+++ EsasyTcpSocket/server.cpp
@@ -3,7 +3,10 @@
 #include<iostream>
 #include<WinSock2.h>
 #include<Windows.h>
+#include<vector>
 
+
+using namespace std;
 
 enum CMD
 {
@@ -66,6 +69,64 @@ struct LogOutResult : public DataHeader
 
 
 
+vector<SOCKET> g_clients;
+
+int processor(SOCKET sockt) 
+{
+	char szRecv[1024] = {};
+	int nLen = recv(sockt, szRecv, sizeof(DataHeader), 0);
+	DataHeader* head = (DataHeader*)szRecv;
+
+	if (nLen <= 0)
+	{
+		cout << "客户端已退出，任务结束" << endl;
+		return -1;
+	}
+
+	switch (head->cmd)
+	{
+	case CMD_LOGIN:
+	{
+
+		recv(sockt, szRecv + sizeof(DataHeader), head->dataLenght - sizeof(DataHeader), 0);
+		Login* login = (Login*)szRecv;
+		std::cout << "登陆操作" << "用户名:" << login->userName << "密码:" << login->password << "命令:" << login->cmd << "数据长度:" << login->dataLenght << std::endl;
+		LoginResult loginResult = { };
+		if (0 == strcmp(login->userName, "caihui") && 0 == strcmp(login->password, "123456"))
+		{
+			std::cout << "登录成功" << std::endl;
+			loginResult.result = 0;
+		}
+		else
+		{
+			std::cout << "登录失败" << std::endl;
+			loginResult.result = -1;
+		}
+		send(sockt, (char*)& loginResult, sizeof(LoginResult), 0);
+		break;
+	}
+	case CMD_LOGOUT:
+	{
+
+		recv(sockt, szRecv + sizeof(DataHeader), head->dataLenght - sizeof(DataHeader), 0);
+		LogOut* logOut = (LogOut*)szRecv;
+		std::cout << "登出操作" << "用户名:" << logOut->usrName << "命令:" << logOut->cmd << "数据长度:" << logOut->dataLenght << std::endl;
+		LogOutResult logOutResult = { };
+		send(sockt, (const char*)& logOutResult, sizeof(LogOutResult), 0);
+		break;
+	}
+
+	default:
+	{
+		head->cmd = CMD_ERROR;
+		head->dataLenght = 0;
+		send(sockt, (const char*)& head, sizeof(DataHeader), 0);
+		break;
+	}
+	}
+
+}
+
 int main()
 {
 	WORD ver = MAKEWORD(2, 2);
@@ -98,70 +159,69 @@ int main()
 	{
 		std::cout << "监听网络端口成功" << std::endl;
 	}
-	//4. accept等待接收客户端连接
-	sockaddr_in clientAddr = {};
-	int nAddrLen = sizeof(sockaddr_in);
-	SOCKET cSock = INVALID_SOCKET;
-	cSock = accept(sock, (sockaddr*)& clientAddr, &nAddrLen);
-	if (INVALID_SOCKET == cSock)
-	{
-		std::cout << "错误，接收到无限客户端socket ..." << std::endl;
-	}
-	std::cout << "新客户端加入：IP = " << inet_ntoa(clientAddr.sin_addr) << std::endl;
+
 
 	while (true)
 	{
 	
-		//5.接受客户端数据
-		
-		char szRecv[1024] = {};
-		recv(cSock, szRecv, sizeof(DataHeader), 0);
-		DataHeader * head = (DataHeader*)szRecv;
-		switch (head->cmd)
+		fd_set fdRead;
+		fd_set fdWrite;
+		fd_set fdExp;
+		FD_ZERO(&fdRead);
+		FD_ZERO(&fdWrite);
+		FD_ZERO(&fdExp);
+		FD_SET(sock, &fdRead);
+		FD_SET(sock, &fdWrite);
+		FD_SET(sock, &fdExp);
+
+		for (int n = (int)g_clients.size() - 1; n >= 0; n--)
 		{
-		case CMD_LOGIN:
-		{
-		
-			recv(cSock, szRecv + sizeof(DataHeader), head->dataLenght - sizeof(DataHeader), 0);
-			Login* login = (Login*)szRecv;
-			std::cout << "登陆操作"  << "用户名:" << login->userName << "密码:" << login->password << "命令:" << login->cmd << "数据长度:"  << login->dataLenght << std::endl;
-			LoginResult loginResult = { };
-			if ( 0 == strcmp(login->userName, "caihui") && 0 == strcmp(login->password, "123456"))
-			{
-				std::cout << "登录成功" << std::endl;
-				loginResult.result = 0;
-			}
-			else
-			{
-				std::cout << "登录失败" << std::endl;
-				loginResult.result = -1;
-			}
-			send(cSock, (char*)& loginResult, sizeof(LoginResult), 0);
-			break;
+			FD_SET(g_clients[n], &fdRead);
 		}
-		case CMD_LOGOUT:
+
+		timeval t = {0, 0};
+		int ret =  select(sock + 1, &fdRead, &fdWrite, &fdRead, &t);
+		if (ret < 0)
 		{
-		
-			recv(cSock, szRecv + sizeof(DataHeader), head->dataLenght - sizeof(DataHeader), 0);
-			LogOut * logOut = (LogOut *)szRecv;
-			std::cout << "登出操作" << "用户名:" << logOut->usrName << "命令:" << logOut->cmd << "数据长度:" << logOut->dataLenght << std::endl;
-			LogOutResult logOutResult = { };
-			send(cSock, (const char*)&logOutResult , sizeof(LogOutResult), 0);
+			std::cout << "select 任务结束" << std::endl;
 			break;
 		}
 
-		default:
+		if (FD_ISSET(sock, &fdRead))
 		{
-			head->cmd = CMD_ERROR;
-			head->dataLenght = 0;
-			send(cSock, (const char *)&head, sizeof(DataHeader), 0);
-			break;
+			FD_CLR(sock, &fdRead);
+			//4. accept等待接收客户端连接
+			sockaddr_in clientAddr = {};
+			int nAddrLen = sizeof(sockaddr_in);
+			SOCKET cSock = INVALID_SOCKET;
+			cSock = accept(sock, (sockaddr*)& clientAddr, &nAddrLen);
+			if (INVALID_SOCKET == cSock)
+			{
+				std::cout << "错误，接收到无限客户端socket ..." << std::endl;
+			}
+			std::cout << "新客户端加入：IP = " << inet_ntoa(clientAddr.sin_addr) << std::endl;
+			g_clients.push_back(cSock);
 		}
+		//5.接受客户端数据
+		for (size_t n = 0 ;  n < fdRead.fd_count;  n++)
+		{
+			if (-1 == processor(fdRead.fd_array[n]))
+			{
+				auto iter = find(g_clients.begin(), g_clients.end(), fdRead.fd_array[n]);
+				if (iter != g_clients.end()) {
+					g_clients.erase(iter);
+				}
+			}
 		}
-		 
+	
 		
 		//6.处理请求
 		
+	}
+
+	for (size_t n = g_clients.size() - 1; n >= 0; n--)
+	{
+		closesocket(g_clients[n]);
 	}
 	//7. 关闭套接字closesocket
 	closesocket(sock);
